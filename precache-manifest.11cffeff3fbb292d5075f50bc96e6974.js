479282edb6db827132269f85d011e0207a6c0e3e
vkstolici <53238847+vkstolici@users.noreply.github.com>
2019-07-24 00:36:43 +0300

    Add files via upload

---
+++ precache-manifest.11cffeff3fbb292d5075f50bc96e6974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f1b72cc63dc70436ea9",
+    "url": "/static/js/main.fcfd0440.chunk.js"
+  },
+  {
+    "revision": "efbd1d68cf3801e31e11",
+    "url": "/static/js/2.e0d7068b.chunk.js"
+  },
+  {
+    "revision": "3f1b72cc63dc70436ea9",
+    "url": "/static/css/main.dc329cbb.chunk.css"
+  },
+  {
+    "revision": "efbd1d68cf3801e31e11",
+    "url": "/static/css/2.cd4c170e.chunk.css"
+  },
+  {
+    "revision": "bc754b83caff0da21f51e22b6ebb0525",
+    "url": "/index.html"
+  }
+];
